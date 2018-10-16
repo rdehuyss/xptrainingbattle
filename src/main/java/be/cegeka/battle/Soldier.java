d6d05c66619f5c4c94c3dea70f8e540be7202cfd
Dehuysser Ronald <ronald.dehuysser@cegeka.com>
2018-10-16 13:38:03 +0200

    First commit

---
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -0,0 +1,10 @@
+package be.cegeka.battle;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Soldier {
+
+    public Soldier fight(Soldier opponent) {
+        throw new NotImplementedException();
+    }
+}
