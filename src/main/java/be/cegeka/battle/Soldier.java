e9ee722e5e4306f238ef4ecd4a589cc8c14fb2c6
Vettas Alexandre <alexandre.vettas@cegeka.com>
2018-10-16 14:01:13 +0200

    us1

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -4,6 +4,16 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Soldier {
 
+    private final String name;
+
+    public Soldier(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public Soldier fight(Soldier opponent) {
         throw new NotImplementedException();
     }
