619ae9a9e3c92525b646b85a0ebe4eed8fa460be
rdehuyss <ronald.dehuysser@gmail.com>
2018-10-16 14:34:38 +0200

    Merge branch 'master' of https://github.com/rdehuyss/xptrainingbattle
    # Conflicts:
    #	.gradle/4.10/fileHashes/fileHashes.bin
    #	.idea/workspace.xml
    #	src/main/java/be/cegeka/battle/Soldier.java

--- src/test/java/WeaponTest.java
+++ src/test/java/WeaponTest.java
@@ -1,4 +1,7 @@
 import be.cegeka.battle.weapon.Axe;
+import be.cegeka.battle.weapon.BareFist;
+import be.cegeka.battle.weapon.Spear;
+import be.cegeka.battle.weapon.Sword;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
@@ -9,5 +12,18 @@ public class WeaponTest {
     @Test
     public void axeHasDamage3() {
         assertThat(new Axe().getDamage()).isEqualTo(3);
+    }
+    @Test
+    public void SwordHasDamage2() {
+        assertThat(new Sword().getDamage()).isEqualTo(2);
+    }
+    @Test
+    public void SpearHasDamage2() {
+        assertThat(new Spear().getDamage()).isEqualTo(2);
+    }
+
+    @Test
+    public void BareFistHasDamage1() {
+        assertThat(new BareFist().getDamage()).isEqualTo(1);
     }
 }
